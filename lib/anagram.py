e9a343a6ff64ca7643298bfccc3d6e8b5a54ba7b
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-20 07:58:41 -0500

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        anagram_list = []
+
+        char_list = sorted([char for char in self.word])
+
+        for anagram in anagrams:
+            if char_list == sorted(anagram):
+                anagram_list.append(anagram)
+                
+        return anagram_list
+
+word = Anagram("listen")
+result = word.match(["listen", "poison", "hello"])
+print(result)
